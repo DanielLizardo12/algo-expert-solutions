ffac5f4c5c18d249d923fd9e8c6abb8e65f8e6ae
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-13 19:28:14 -0600

    added validate sub sequence solution

---
+++ src/test/java/easy/ValidateSubSequenceTest.java
@@ -0,0 +1,17 @@
+package easy;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Daniel
+ * @Date 13/04/2023
+ */
+
+class ValidateSubSequenceTest {
+
+    @Test
+    public void testValidateSubSequenceSimpleSequence() {
+        
+    }
+
+}
