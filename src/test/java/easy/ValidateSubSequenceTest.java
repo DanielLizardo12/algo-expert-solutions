6243abd50aa040ef626875ee1f414e56eb9eb420
Daniel Lizardo <wdaniel.lizardo1@gmail.com>
2023-04-13 19:29:56 -0600

    added unit testing for validatesub sequence

--- src/test/java/easy/ValidateSubSequenceTest.java
+++ src/test/java/easy/ValidateSubSequenceTest.java
@@ -1,6 +1,10 @@
 package easy;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Daniel
@@ -10,8 +14,35 @@ import org.junit.jupiter.api.Test;
 class ValidateSubSequenceTest {
 
     @Test
-    public void testValidateSubSequenceSimpleSequence() {
-        
+    public void testValidateSubSequenceSimpleInnerArray() {
+        List<Integer> array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+
+        List<Integer> innerArray = new ArrayList<>();
+        innerArray.add(2);
+        innerArray.add(4);
+
+        Assertions.assertTrue(ValidateSubSequence.isValidSubsequence(array, innerArray));
+    }
+
+    @Test
+    public void testValidateSubSequenceSimpleInnerArrayWrongOrder() {
+        List<Integer> array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+        array.add(5);
+
+        List<Integer> innerArray = new ArrayList<>();
+        innerArray.add(4);
+        innerArray.add(2);
+
+        Assertions.assertFalse(ValidateSubSequence.isValidSubsequence(array, innerArray));
     }
 
 }
